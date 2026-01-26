6b61a0faf0d0c210660cdecfd49c0e9f980d0ad9
mariarubiosamaniego <mariarubiosa2006@gmail.com>
2026-01-26 17:44:55 +0100

    sessions5&6If

--- sessions56/operators.py
+++ sessions56/operators.py
@@ -3,6 +3,12 @@ print(3*2)  #multiplication
 print(8/2)  #division, always return float (we have decimals always even though it is exact
 print(8//2) #if you want integer result
 print(9%2)  #modulo (how many times can i fit the first number inside the 2º
+print(3**2)  #power
+
+print(4 == 3) #comparison
+print(3 != 4) #not equal
+print(3<100)
+print(3<=3)
 
 print(True or False) #True
 print(True and False) #False
@@ -20,3 +26,8 @@ print(not 7)
 print("")
 
 #everything in Python is considered True except for 7 items -> 0, 0.0, False, None, empty list (), [], "", {}
+
+
+#ALONE EXERCISES
+print((26**3) > (10**4))
+#what is greater when naming spanish cars, the 3 letter combo or the 4 digit combo. what give you more combinations
