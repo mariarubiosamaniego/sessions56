8065b97ba7cda8e40219695c1376dff9ed38a7f2
mariarubiosamaniego <mariarubiosa2006@gmail.com>
2026-01-26 16:12:30 +0100

    sessions5&6

--- sessions56/operators.py
+++ sessions56/operators.py
@@ -1,4 +1,22 @@
 print(2-3)  #assition or substraction
 print(3*2)  #multiplication
 print(8/2)  #division, always return float (we have decimals always even though it is exact
-print(8//2)  #if you want integer result
+print(8//2) #if you want integer result
+print(9%2)  #modulo (how many times can i fit the first number inside the 2º
+
+print(True or False) #True
+print(True and False) #False
+
+print(7 or 6)
+print(7 and 6)
+print(0 or None or False or 22 or 0.8) #result 22
+print(11 and 22 and None and 17)
+print(7 and 8 and 9 and 10)
+print(0 or None or False or 0.0 or [])
+print((6 or 9) and ("Bob" or "Joe"))
+
+#not is always True or False
+print(not 7)
+print("")
+
+#everything in Python is considered True except for 7 items -> 0, 0.0, False, None, empty list (), [], "", {}
