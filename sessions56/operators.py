cdee586738cdd830af0409edaa086c93cff672aa
mariarubiosamaniego <mariarubiosa2006@gmail.com>
2026-01-26 15:42:07 +0100

    sessions5&6

---
+++ sessions56/operators.py
@@ -0,0 +1,4 @@
+print(2-3)  #assition or substraction
+print(3*2)  #multiplication
+print(8/2)  #division, always return float (we have decimals always even though it is exact
+print(8//2)  #if you want integer result
